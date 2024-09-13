77c1219d32b2fbd68fee5aa3b4fe61f756f0c156
varun0211 <22236537+varun0211@users.noreply.github.com>
2024-09-13 09:08:33 +0530

    Create outputs.tf

---
+++ Terraform/s3_backend/outputs.tf
@@ -0,0 +1,14 @@
+# output "s3_backend_id" {
+#   value = module.s3_backend.id
+#   description = "The name of the bucket."
+# }
+
+# output "s3_backend_hosted_zone" {
+#   value = module.s3_backend.hosted_zone_id
+#   description = "The Hosted zone of the bucket."
+# }
+
+# output "dynamodb_name" {
+#   value = module.dynamodb_locking.name
+#   description = "The name of the bucket."
+# }
